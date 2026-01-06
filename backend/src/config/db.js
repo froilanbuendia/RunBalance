47f162424047ed43c0a59dee3c753e7833e567bd
froilanbuendia <froilangbuendia@gmail.com>
2026-01-05 22:05:01 -0800

    database config

---
+++ backend/src/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  DB_USER: process.env.DB_USER,
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_PWD: process.env.DB_PWD,
+  DB_PORT: process.env.DB_PORT,
+};
