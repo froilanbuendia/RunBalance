b6cc4a64dd6747f073d00d3e63d069d14f14ac99
froilanbuendia <froilangbuendia@gmail.com>
2026-03-31 17:31:19 -0700

    camelCase fix

--- backend/src/routes/healthCheck.js
+++ backend/src/routes/healthCheck.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const healthCheckController = require("../controllers/healthcheckController");
+const healthCheckController = require("../controllers/healthCheckController");
 
 const router = express.Router();
 
